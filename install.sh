68ff956255fb251c5bb7b8e84c87f9c3e526acac
Joshua Moore <40312633+joshuamoorexyz@users.noreply.github.com>
2021-08-01 03:48:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ make install
 srsran_install_configs.sh user
 
 
-#start install gnuradio 3.7 from source
+#start install gnuradio 3.8 from source
 cd ~/WorkArea/
 apt install git cmake g++ libboost-all-dev libgmp-dev swig python3-numpy python3-mako python3-sphinx python3-lxml doxygen libfftw3-dev libsdl1.2-dev libgsl-dev libqwt-qt5-dev libqt5opengl5-dev python3-pyqt5 liblog4cpp5-dev libzmq3-dev python3-yaml python3-click python3-click-plugins python3-zmq python3-scipy python3-pip python3-gi-cairo -y
 
